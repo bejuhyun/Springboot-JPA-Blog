9677327bc2a6fccc63822120f4acfa4ecb6c6485
JUHYUN JUNG <bejuhyun@gmail.com>
2022-03-29 21:39:08 +0900

    환경세팅 완료

---
+++ src/main/java/com/zxcv/blog/test/TempControllerTest.java
@@ -0,0 +1,15 @@
+package com.zxcv.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+	
+	@GetMapping("/temp/home")
+	public String tempHome() {
+		System.out.println("temp-home");
+		return "/home.html";
+	}
+
+}
